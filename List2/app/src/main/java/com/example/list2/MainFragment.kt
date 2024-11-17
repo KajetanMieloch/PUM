e323a5d9fb18fc220c87e5d7e730ac56268acee2
Kajetan <kajetanmieloch@gmail.com>
2024-11-17 14:14:08 +0100

    List2 nav done

---
+++ List2/app/src/main/java/com/example/list2/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.list2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonToLogin = view.findViewById<Button>(R.id.button_to_login)
+        buttonToLogin.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment)
+        }
+    }
+}
