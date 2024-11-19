e53843013ec89738adf2feba7e7471b525b5d96d
Kajetan <kajetanmieloch@gmail.com>
2024-11-19 11:53:08 +0100

    List2 done

---
+++ Lista3/app/src/main/java/com/example/lista3/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.list3
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginButton = view.findViewById<Button>(R.id.button_to_login)
+        val registerButton = view.findViewById<Button>(R.id.button_to_register)
+
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
+
+        registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_registerFragment)
+        }
+    }
+}
